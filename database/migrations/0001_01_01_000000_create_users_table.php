91f70275fdcf89845feb9c4ae3557be8cade03bf
nwiuser <nouisserzakariae@gmail.com>
2024-07-25 11:34:03 +0100

    migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('role');
+            $table->string('google_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
